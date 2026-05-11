9a7be9b7c28b951aceb2c30ba682a84cf1e5d187
bernardosvent <bernardosvent@gmail.com>
2026-05-11 20:07:37 -0300

    feat: estrutura inicial do projeto Trabalho3 Lakehouse Databricks

---
+++ git_setup_t3.ps1
@@ -0,0 +1,7 @@
+cd "C:\Users\berna\OneDrive\Área de Trabalho\Trabalho3-Lakehouse-Databricks"
+git init
+git remote add origin https://github.com/bernardosvent/Trabalho3-Lakehouse-Databricks.git
+git pull origin main --allow-unrelated-histories
+git add .
+git commit -m "feat: estrutura inicial do projeto Trabalho3 Lakehouse Databricks"
+git push -u origin main
